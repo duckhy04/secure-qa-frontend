673fe07524986264011dca2a4d2e606e3f32bcee
duckhy04 <nguynduchuy12@gmail.com>
2024-11-28 16:10:57 +0700

    Add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router";
+
+export const SharedModule = [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+]
