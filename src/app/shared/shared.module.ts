5e40a9d55241b88f5b5d52b389d86ddf3486d9c7
duckhy04 <nguynduchuy12@gmail.com>
2024-11-29 03:08:17 +0700

    Update shared modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,17 @@ import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule } from "@angular/router";
+import { ButtonComponent } from "./components/button/button.component";
+import { ToastComponent } from "./components/toast/toast.component";
 
 export const SharedModule = [
+    // Modules
     CommonModule,
     FormsModule,
     RouterModule,
     ReactiveFormsModule,
+
+    // Components
+    ButtonComponent,
+    ToastComponent,
 ]
