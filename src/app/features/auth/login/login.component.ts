a0c1d305d2560e8a368b23579f643338f2b153e1
duckhy04 <nguynduchuy12@gmail.com>
2024-11-29 03:06:49 +0700

    Update feature login

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService, StorageService, ToastService } from '../../../core/services/services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,15 +14,51 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class LoginComponent {
 
   loginForm!: FormGroup
+  loginError: string | null = null;
 
-  constructor(private fb: FormBuilder) {
-    
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private localStorage: StorageService,
+    private toastService: ToastService,
+    private router: Router,
+  ) {
+
   }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      
+      username: [null, Validators.required],
+      password: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(/[A-Z]/),
+          Validators.pattern(/[a-z]/),
+          Validators.pattern(/[0-9]/),
+        ],
+      ],
+    });
+  }
+
+  login() {
+
+    // console.log(this.loginForm.value);
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        // console.log(response.data.token);
+        this.localStorage.saveToken(response.data.token);
+        this.toastService.showToast(response.message, 'success');
+        this.loginError = null;
+        this.router.navigate(['/home']);
+      },
+      error: (error) => {
+        this.loginError = error.error.message;
+        this.toastService.showToast(error.error.message, 'error');
+      }
     })
+
   }
 
 }
