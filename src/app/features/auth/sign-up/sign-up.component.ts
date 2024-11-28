75ae21c8bdff0cf522a9625bca439c68eaa24054
duckhy04 <nguynduchuy12@gmail.com>
2024-11-28 22:48:06 +0700

    Add ui sign up

---
+++ src/app/features/auth/sign-up/sign-up.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
+})
+export class SignUpComponent {
+
+}
