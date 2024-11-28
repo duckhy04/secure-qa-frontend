f707bd2cb56c7ec1c173f7c47a9fd7fac72b67ed
duckhy04 <nguynduchuy12@gmail.com>
2024-11-28 19:16:23 +0700

    Add colors/plugins config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'colors': {
+        'dodger-blue': '#1E90FF',
+        'dark-grey': '#A9A9A9',
+        'canary-yellow': '#FFEF00',
+        'sea-green': '#2EB857',
+        'blue-violet': '#8A2BE2',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none', /* IE and Edge */
+          'scrollbar-width': 'none', /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none', /* Chrome, Safari, Edge */
+        },
+      });
+    },
+  ],
 }
