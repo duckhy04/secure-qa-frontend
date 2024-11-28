253005e2f3538be86cc32124047b236d4a8a11c9
duckhy04 <nguynduchuy12@gmail.com>
2024-11-29 03:08:51 +0700

    Update app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './layouts/auth/auth.component';
 import { MainComponent } from './layouts/main/main.component';
+import { roleGuard } from './core/guards/role/role.guard';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,12 @@ export const routes: Routes = [
             {
                 path: 'home',
                 loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent),
+            },
+            {
+                data: { expectedRole: '[ROLE_ADMIN]' },
+                canActivate: [roleGuard],
+                path: 'user-manager',
+                loadComponent: () => import('./features/user-manager/user-manager.component').then(m => m.UserManagerComponent),
             },
         ]
     },
@@ -25,6 +32,11 @@ export const routes: Routes = [
                 path: 'sign-up',
                 loadComponent: () => import('./features/auth/sign-up/sign-up.component').then(m => m.SignUpComponent)
             },
+            {
+                path: 'not-authorized',
+                loadComponent: () => import('./features/auth/not-authorize/not-authorize.component').then(m => m.NotAuthorizeComponent)
+            }
         ]
-    }
+    },
+
 ];
