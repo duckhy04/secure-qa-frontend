51bb7fd59f89fb2cc3fa175cdb00ffe154770a88
duckhy04 <nguynduchuy12@gmail.com>
2024-11-29 03:07:06 +0700

    Add not authorized component

---
+++ src/app/features/auth/not-authorize/not-authorize.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-authorize',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-authorize.component.html',
+  styleUrl: './not-authorize.component.scss'
+})
+export class NotAuthorizeComponent {
+
+}
