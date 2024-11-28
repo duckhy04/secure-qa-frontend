edf2647828c437c2bb42baaa5ca7b69863ec07d5
duckhy04 <nguynduchuy12@gmail.com>
2024-11-28 22:49:29 +0700

    Update main layout

---
+++ src/app/layouts/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+  isAuthentication: boolean = false;
+
+}
