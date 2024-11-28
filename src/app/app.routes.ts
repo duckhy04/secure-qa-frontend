ed5ed4af452ff0fdb0dc6189a69414fb5da58770
duckhy04 <nguynduchuy12@gmail.com>
2024-11-28 22:50:06 +0700

    Update router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,29 @@
 import { Routes } from '@angular/router';
-import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { MainComponent } from './layouts/main/main.component';
 
 export const routes: Routes = [
     {
         path: '', 
-        component: MainLayoutComponent,
+        component: MainComponent,
         children: [
             {
                 path: 'home',
                 loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent),
+            },
+        ]
+    },
+    {
+        path: 'auth',
+        component: AuthComponent,
+        children: [
+            {
+                path: 'login',
+                loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
+            },
+            {
+                path: 'sign-up',
+                loadComponent: () => import('./features/auth/sign-up/sign-up.component').then(m => m.SignUpComponent)
             },
         ]
     }
